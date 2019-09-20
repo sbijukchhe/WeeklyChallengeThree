135cc6136a43ad0903cb9877f02312092497c3b2
sbijukchhe <skbijukchhe@gmail.com>
2019-09-19 21:52:54 -0400

    Weekly Challenge Three - Making Resume

--- src/ResumeApp.java
+++ src/ResumeApp.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class ResumeApp {
     public static void main(String[] args){
 
+        String response = "Y";
+
         Scanner input = new Scanner(System.in);
         Skill coreSkill = new Skill();
         coreSkill.setSkillName("Core Skills");
@@ -20,82 +22,119 @@ public class ResumeApp {
         String emailAdd = input.nextLine();
 
         // Request for Education details
-        // First degree
-        System.out.println("Please enter your Degree type - Associate's, Bachelor's, Master's, PhD, etc. ");
-        String degreeType = input.nextLine();
-        System.out.println("Please enter your major ");
-        String major = input.nextLine();
-        System.out.println("Please enter your University name ");
-        String univName = input.nextLine();
-        System.out.println("Please enter your Graduation year ");
-        String gradYear = input.nextLine();
+        System.out.println("Do you want to enter your Education details? Y / N");
+        response = input.nextLine();
+        while (response.equalsIgnoreCase("Y")) {
+//            System.out.println("Do you want to enter your Education details? Y / N");
+//            response = input.nextLine();
+            if (response.equalsIgnoreCase("Y")) {
+                // First degree
+                System.out.println("Please enter your Degree type - Associate's, Bachelor's, Master's, PhD, etc. ");
+                String degreeType = input.nextLine();
+                System.out.println("Please enter your major ");
+                String major = input.nextLine();
+                System.out.println("Please enter your University name ");
+                String univName = input.nextLine();
+                System.out.println("Please enter your Graduation year ");
+                String gradYear = input.nextLine();
 
-        Education educationOne = new Education();
-        educationOne.setDegreeType(degreeType);
-        educationOne.setMajor(major);
-        educationOne.setUniversityName(univName);
-        educationOne.setGraduationYear(gradYear);
-        education.addEducation(educationOne);
+                Education educationOne = new Education();
+                educationOne.setDegreeType(degreeType);
+                educationOne.setMajor(major);
+                educationOne.setUniversityName(univName);
+                educationOne.setGraduationYear(gradYear);
+                education.addEducation(educationOne);
+            }
+            else{
+                break;
+            }
+            System.out.println("Do you want to enter your Education details? Y / N");
+            response = input.nextLine();
+        }
 
         // Request for Work experience
-        System.out.println("Please enter your Job title ");
-        String jTitle = input.nextLine();
-        System.out.println("Please enter your Company name ");
-        String cName = input.nextLine();
-        System.out.println("Please enter your job Start date - month year (eg September 2019)");
-        String startDate = input.nextLine();
-        System.out.println("Please enter your job End date - month year (eg September 2019)");
-        String endDate = input.nextLine();
-        System.out.println("Please enter Job description ");
-        String jobDescription = input.nextLine();
+       System.out.println("Do you want to enter your Work Experience? Y / N");
+        response = input.nextLine();
+        while (response.equalsIgnoreCase("Y")) {
+            //System.out.println("Do you want to enter your Work Experience? Y / N");
+            //response = input.nextLine();
+            if (response.equalsIgnoreCase("Y")) {
+                System.out.println("Please enter your Job title ");
+                String jTitle = input.nextLine();
+                System.out.println("Please enter your Company name ");
+                String cName = input.nextLine();
+                System.out.println("Please enter your job Start date - month year (eg September 2019)");
+                String startDate = input.nextLine();
+                System.out.println("Please enter your job End date - month year (eg September 2019)");
+                String endDate = input.nextLine();
+                System.out.println("Please enter Job description ");
+                String jobDescription = input.nextLine();
 
-        WorkExperience workExperienceOne = new WorkExperience();
-        workExperienceOne.setJobTitle(jTitle);
-        workExperienceOne.setCompany(cName);
-        workExperienceOne.setStartDate(startDate);
-        workExperienceOne.setEndDate(endDate);
-        workExperienceOne.setJobDescription(jobDescription);
-        workExperience.addExperience(workExperienceOne);
+                WorkExperience workExperienceOne = new WorkExperience();
+                workExperienceOne.setJobTitle(jTitle);
+                workExperienceOne.setCompany(cName);
+                workExperienceOne.setStartDate(startDate);
+                workExperienceOne.setEndDate(endDate);
+                workExperienceOne.setJobDescription(jobDescription);
+                workExperience.addExperience(workExperienceOne);
+            }
+            else{
+                break;
+            }
+            System.out.println("Do you want to enter your Work Experience? Y / N");
+            response = input.nextLine();
+        }
 
+        System.out.println("Do you want to enter your skills? Y / N");
+        response = input.nextLine();
 
         // Request the name of skills
-        System.out.println("Please enter your skills ");
+        //System.out.println("Please enter your skills ");
+        //input.nextLine();
+        while (response.equalsIgnoreCase("Y")) {
+//            System.out.println("Do you want to enter your skills? Y / N");
+//            response = input.nextLine();
+            if (response.equalsIgnoreCase("Y")) {
 
-        // first skill
-        System.out.println("Please enter your first skill name");
-        String skillNameOne = input.nextLine();
-        System.out.println("Please enter your first skill proficiency");
-        System.out.println("Fundamental, Novice, Intermediate, Advanced or Expert");
-        String skillProfOne = input.nextLine();
+                // first skill
+                System.out.println("Please enter your skill name");
+                String skillNameOne = input.nextLine();
+                System.out.println("Please enter your skill proficiency");
+                System.out.println("Fundamental, Novice, Intermediate, Advanced or Expert");
+                String skillProfOne = input.nextLine();
 
-        Skill skillOne = new Skill();
-        skillOne.setSkillName(skillNameOne);
-        skillOne.setProficiency(skillProfOne);
-        coreSkill.addSkill(skillOne);
-
-        // second skill
-        System.out.println("Please enter your second skill name");
-        String skillNameTwo = input.nextLine();
-        System.out.println("Please enter your second skill proficiency");
-        System.out.println("Fundamental, Novice, Intermediate, Advanced or Expert");
-        String skillProfTwo = input.nextLine();
-
-        Skill skillTwo = new Skill();
-        skillTwo.setSkillName(skillNameTwo);
-        skillTwo.setProficiency(skillProfTwo);
-        coreSkill.addSkill(skillTwo);
-
-        // third skill
-        System.out.println("Please enter your third skill name");
-        String skillNameThree = input.nextLine();
-        System.out.println("Please enter your third skill proficiency");
-        System.out.println("Fundamental, Novice, Intermediate, Advanced or Expert");
-        String skillProfThree = input.nextLine();
-
-        Skill skillThree = new Skill();
-        skillThree.setSkillName(skillNameThree);
-        skillThree.setProficiency(skillProfThree);
-        coreSkill.addSkill(skillThree);
+                Skill skillOne = new Skill();
+                skillOne.setSkillName(skillNameOne);
+                skillOne.setProficiency(skillProfOne);
+                coreSkill.addSkill(skillOne);
+            }
+              System.out.println("Do you want to enter your skills? Y / N");
+              response = input.nextLine();
+        }
+//
+//        // second skill
+//        System.out.println("Please enter your second skill name");
+//        String skillNameTwo = input.nextLine();
+//        System.out.println("Please enter your second skill proficiency");
+//        System.out.println("Fundamental, Novice, Intermediate, Advanced or Expert");
+//        String skillProfTwo = input.nextLine();
+//
+//        Skill skillTwo = new Skill();
+//        skillTwo.setSkillName(skillNameTwo);
+//        skillTwo.setProficiency(skillProfTwo);
+//        coreSkill.addSkill(skillTwo);
+//
+//        // third skill
+//        System.out.println("Please enter your third skill name");
+//        String skillNameThree = input.nextLine();
+//        System.out.println("Please enter your third skill proficiency");
+//        System.out.println("Fundamental, Novice, Intermediate, Advanced or Expert");
+//        String skillProfThree = input.nextLine();
+//
+//        Skill skillThree = new Skill();
+//        skillThree.setSkillName(skillNameThree);
+//        skillThree.setProficiency(skillProfThree);
+//        coreSkill.addSkill(skillThree);
 
 
 
@@ -109,7 +148,7 @@ public class ResumeApp {
         System.out.println(emailAdd);
 
         // Printing education details
-        System.out.println("\n\nEducation");
+        System.out.println("\nEducation");
         System.out.println("-----------");
         ArrayList<String> eduDetails  = new ArrayList<>();
         for(Education edu : education.getEducations())
@@ -118,12 +157,12 @@ public class ResumeApp {
             eduDetails.add(edu.major);
             eduDetails.add(edu.universityName);
             eduDetails.add(edu.graduationYear);
-            System.out.println(edu.getDegreeType()+ " in " + edu.getMajor()+ ", \n" + edu.getUniversityName() +
+            System.out.println("\n" + edu.getDegreeType()+ " in " + edu.getMajor()+ ", \n" + edu.getUniversityName() +
                     ", " + edu.getGraduationYear());
         }
 
         // Printing Work Experience
-        System.out.println("\n\nExperience");
+        System.out.println("\nExperience");
         System.out.println("-----------");
         ArrayList<String> experienceDetails  = new ArrayList<>();
         for(WorkExperience wExperience : workExperience.getWorkExperiences())
@@ -139,17 +178,13 @@ public class ResumeApp {
                     wExperience.getJobDescription());
         }
         // printing skills
-        System.out.println("\n\nSkills");
+        System.out.println("\nSkills");
         System.out.println("-----------");
         ArrayList<String> skillSets  = new ArrayList<>();
         for(Skill sk : coreSkill.getSkills())
         {
             skillSets.add(sk.getSkillName());
             skillSets.add(sk.getProficiency());
-//            System.out.println(coreSkill.getSkillName()+", \n "+sk.getSkillName()+
-//                    "\t and Proficiency is :" + sk.getProficiency());
-
-
         System.out.println(sk.getSkillName()+ ",\t " + sk.getProficiency());
         }
     }
